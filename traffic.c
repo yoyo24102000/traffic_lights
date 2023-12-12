749b83e4ee4db253c76b7a608ffbc0052c2ca33c
yoyo24102000 <151637504+yoyo24102000@users.noreply.github.com>
2023-12-12 15:20:56 +0100

    Update traffic.c

--- traffic.c
+++ traffic.c
@@ -3,36 +3,33 @@
 #include <assert.h>
 #include <stddef.h>
 
-#define LIGHT_MASK 0x0F
-#define SHIFT_MASK 0x01
-
 void init(unsigned char *lights) {
-
+   
     *lights = 0;
 }
 
 void turn_on(unsigned char *lights, unsigned char light_num) {
-    
+
     *lights |= (1 << light_num);
 }
 
 void turn_off(unsigned char *lights, unsigned char light_num) {
-    
+  
     *lights &= ~(1 << light_num);
 }
 
 void next_step(unsigned char *lights) {
-  
-    *lights = (*lights << 1) ^ ((*lights & (1 << 3)) ? SHIFT_MASK : 0);
+   
+    *lights = (*lights << 1) | ((*lights & 0x08) >> 3);
 }
 
 void reverse(unsigned char *lights) {
-    
-    *lights ^= LIGHT_MASK;
+
+    *lights ^= 0x0F;
 }
 
 void swap(unsigned char *lights_1, unsigned char *lights_2) {
-   
+
     *lights_1 ^= *lights_2;
     *lights_2 ^= *lights_1;
     *lights_1 ^= *lights_2;
